661a2a3ba1e4b2563b7a51c409f22e576b0f1f1b
abberdin <abdurahim.khudoyberdiev@gmail.com>
2020-02-04 23:20:02 +0500

    Added GeoLocation model

---
+++ Ip2LocationAPI/Models/Geolocation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Ip2LocationAPI.Models
+{
+    public class GeoLocation
+    {
+        [JsonIgnore]
+        public int GeonameId { get; internal set; }
+
+        [JsonPropertyName("postal_code")]
+        public string PostalCode { get; internal set; }
+
+        [JsonPropertyName("latitude")]
+        public string Latitude { get; internal set; }
+
+        [JsonPropertyName("longitude")]
+        public string Longitude { get; internal set; }
+
+        [JsonPropertyName("accuracy_radius")]
+        public string AccuracyRadius { get; internal set; }
+
+    }
+}
